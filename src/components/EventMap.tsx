2e199d4ecffa62eb90508a9dcdd30a1bc6758fd4
ccozens <ccozens@gmail.com>
2023-05-02 12:41:35 +0100

    added google map integration and styled event page

--- src/components/EventMap.tsx
+++ src/components/EventMap.tsx
@@ -1,42 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	GoogleMap,
 	LoadScript,
-	Marker,
+	MarkerF,
 } from '@react-google-maps/api';
 const containerStyle = {
-	width: '400px',
-	height: '400px',
-};
-const westernPark = {
-	lat: 52.63304433542604,
-	lng: -1.1752956903540588,
-};
-
-const braunstoneLeisureCentre = {
-	lat: 52.631370843015894,
-	lng: -1.17763457645657,
+	width: '300px',
+	minWidth: '232px',
+	aspectRatio: '1/1',
+	borderRadius: '8px',
+	border: '10px solid #F7E1A1;',
+	margin: '1rem auto',
 };
 
-function EventMap() {
+type LocationProps = {
+	label: string;
+	lat: number;
+	lng: number;
+};
+
+function EventMap({ location }: { location: LocationProps }) {
 	const localGoogleMapsApiKey =
 		process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '';
 
+	// open google maps window if short click (ie not id user panning around by click and drag)
+	const [clickTime, setClickTime] = React.useState(0);
+	const onMapMouseDown = () => {
+		setClickTime(Date.now());
+	};
+
+	const onMapClick = React.useCallback(
+		(e: React.SyntheticEvent) => {
+			// do nothing on mouse down
+			e.preventDefault();
+			// if mousedown duration is less than 100ms, open google maps
+			const duration = Date.now() - clickTime;
+			if (duration < 100) {
+				window.open(
+					`https://maps.google.com?q=${location.label}`,
+					'_blank'
+				);
+			}
+		},
+		[clickTime, location]
+	);
+
 	return (
-		<div>
-			{' '}
-			map?
+		<div
+			className="eventMap"
+			onMouseDown={onMapMouseDown}
+			onMouseUp={onMapClick}>
 			<LoadScript googleMapsApiKey={localGoogleMapsApiKey}>
 				<GoogleMap
 					mapContainerStyle={containerStyle}
-					center={westernPark}
-					zoom={12}>
-					{}
+					center={location}
+					zoom={14}>
+					<MarkerF position={location} />
 				</GoogleMap>
-				<Marker
-					label="Braunstone Leisure Centre"
-					position={braunstoneLeisureCentre}
-				/>
 			</LoadScript>
 		</div>
 	);
--- src/pages/events/[id].tsx
+++ src/pages/events/[id].tsx
@@ -4,6 +4,7 @@ import { Event } from '@prismatypes';
 import styles from '@/styles/EventPage.module.css';
 import EventMap from '@/components/EventMap';
 import GeoLocation from '@/components/GeoLocation';
+import { useState } from 'react';
 
 export const getServerSideProps: GetServerSideProps = async (
 	context
@@ -21,25 +22,87 @@ export const getServerSideProps: GetServerSideProps = async (
 	};
 };
 
+type LocationProps = {
+	label: string;
+	lat: number;
+	lng: number;
+};
+
 export default function EventPage({ event }: { event: Event }) {
-	const { name, description, cost, termTime } = event;
+	const {
+		name,
+		description,
+		cost,
+		termTime,
+		email,
+		phone,
+		website,
+		updatedAt,
+	} = event;
 	const ageRange = `${event.minAge} - ${event.maxAge}`;
+	const eventUpdated = new Date(updatedAt).toLocaleDateString(
+		'en-GB'
+	);
+
+	const braunstoneLeisureCentre = {
+		label: 'Braunstone Leisure Centre',
+		lat: 52.631370843015894,
+		lng: -1.17763457645657,
+	};
+
+	const dmuPool = {
+		label: 'Queen Elizabeth II Diamond Jubilee Leisure Centre',
+		lat: 52.629,
+		lng: -1.14,
+	};
+
+	const [location, setLocation] = useState(dmuPool);
 
 	return (
-		<div>
-
-			<div className={styles.eventSummary}>
-				{name}
-				{description}
-				{ageRange}
-				{/* {location} */}
-				{termTime ? 'Term time only' : 'All year'}
-				{cost ? `£${cost}` : 'Free'}
+		<div className={styles.event}>
+			<div className={styles.eventContainer}>
+				<div className={styles.eventSummary}>
+					<div className={styles.eventHead}>
+						<p className={styles.eventTitle}>{name}</p>
+						<p className={styles.eventText}>{description}</p>
+						{/* <p className={styles.eventText}> {location}</p> */}
+						<p className={styles.eventText}>
+							{termTime ? 'Term time only' : 'Runs all year'}
+						</p>
+					</div>
+					<div className={styles.eventGrid}>
+						<p className={styles.eventText}>Age range: </p>
+						<p className={styles.eventText}>{ageRange}</p>
+						<p className={styles.eventText}>Cost: </p>
+						<p className={styles.eventText}>
+							{cost ? `£${cost}` : 'Free'}
+						</p>
+						<p className={styles.eventText}>Website: </p>
+						<p className={styles.eventText}>
+							{website ? `${website}` : ''}
+						</p>
+						<p className={styles.eventText}>Email: </p>
+						<p className={styles.eventText}>
+							{email ? `${email}` : ''}
+						</p>
+						<p className={styles.eventText}>Phone: </p>
+						<p className={styles.eventText}>
+							{phone ? `${phone}` : ''}
+						</p>
+					</div>
+				</div>
+				<div className={styles.eventSummary}>
+					<p className={styles.eventHead}>{location.label}</p>
+					<EventMap location={location} />
+				</div>
 			</div>
-			{/* <GeoLocation /> */}
-			<EventMap />
-
-				<div>Details Location Website</div>
+			<div className={styles.eventFooter}>
+				<p className={styles.footerText}>
+					Event last updated: {eventUpdated}
+				</p>
+				<p className={styles.footerButtons}>Edit event</p>
+				<p className={styles.footerButtons}>Delete event</p>
 			</div>
+		</div>
 	);
 }
