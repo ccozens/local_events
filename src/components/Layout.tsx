4714d9d5087ec9fe64586fb70e607a96d0849bc0
ccozens <ccozens@gmail.com>
2023-05-05 13:15:00 +0100

    amended hero image to priorty={true}

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -58,7 +58,7 @@ export default function Layout({ children }: Children) {
 						maxWidth: '100%',
 						height: 'auto',
 					}}
-					priority
+					priority={true}
 				/>
 			</section>
 			<main> {children} </main>
