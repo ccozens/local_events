eab8962c5de04f54876321ea336c323150f83e5b
ccozens <ccozens@gmail.com>
2023-05-15 12:39:33 +0100

    updated LocationForForm type to allow all properties to be null or undefined, as per comment

--- src/types/LocationForForm.ts
+++ src/types/LocationForForm.ts
@@ -1,6 +1,14 @@
 import { Location } from '@prismatypes';
 
-export type LocationForForm = Omit<Location, 'id' | 'createdAt' | 'updatedAt'> & {
+type StringOrUndefined<T> = {
+	[P in keyof T]: T[P] | undefined;
+};
+
+// map id, createdAt, updatedAt to string or null and all other fields to string or undefined
+export type LocationForForm = Omit<
+	StringOrUndefined<Location>,
+	'id' | 'createdAt' | 'updatedAt'
+> & {
 	id?: number | null;
 	createdAt?: string | null;
 	updatedAt?: string | null;
