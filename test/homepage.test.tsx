de4815fc253b50649375d19eeb338c05d92dd1a3
ccozens <ccozens@gmail.com>
2023-05-18 13:15:43 +0100

    removed testing library and vitest so can focus on playwright

--- test/homepage.test.tsx
+++ test/homepage.test.tsx
@@ -5,33 +5,30 @@ import React from 'react';
 
 // import modules to test
 // import home module from pages/index.tsx
-import Home from '../src/pages/index';
+import Home from '../../src/pages/index';
 // import { getStaticPaths } from '@/pages/index';
-import EventCard from '../src/components/EventCard';
+import EventCard from '../../src/components/EventCard';
 import { mockEvent, mockEvents } from './mockData/mockEvent';
 
+describe('Homepage testing', () => {
+	test('daysOfWeekGrid renders', () => {
+		render(<Home events={mockEvents} />);
+		expect(screen.getByText('Monday')).toBeInTheDocument();
+		expect(screen.getByText('Tuesday')).toBeInTheDocument();
+		expect(screen.getByText('Wednesday')).toBeInTheDocument();
+		expect(screen.getByText('Thursday')).toBeInTheDocument();
+	});
 
-describe ('Homepage testing', () => {
-
-    test('daysOfWeekGrid renders', () => {
-        render(<Home events={mockEvents} />);
-        expect(screen.getByText('Monday')).toBeInTheDocument();
-        expect(screen.getByText('Tuesday')).toBeInTheDocument();
-        expect(screen.getByText('Wednesday')).toBeInTheDocument();
-        expect(screen.getByText('Thursday')).toBeInTheDocument();
-
-    });
-
-
-    test ('EventCard renders', () => {  
-    
-        render(<EventCard event={mockEvent}/>);
-        const mockEventName: string = mockEvent.name;
-        const mockEventLocation: string = mockEvent.location.name;
-        screen.logTestingPlaygroundURL();
-        // tests 
-        expect(screen.getByText(mockEventName)).toBeInTheDocument();
-        // expect(screen.getByText(mockEventLocation)).toBeInTheDocument();   
-        expect(screen.getByText(new RegExp(`venue:${mockEventLocation}`, 'i')));
-     });
+	test('EventCard renders', () => {
+		render(<EventCard event={mockEvent} />);
+		const mockEventName: string = mockEvent.name;
+		const mockEventLocation: string = mockEvent.location.name;
+		screen.logTestingPlaygroundURL();
+		// tests
+		expect(screen.getByText(mockEventName)).toBeInTheDocument();
+		// expect(screen.getByText(mockEventLocation)).toBeInTheDocument();
+		expect(
+			screen.getByText(new RegExp(`venue:${mockEventLocation}`, 'i'))
+		);
+	});
 });
