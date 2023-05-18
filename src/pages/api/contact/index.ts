889a57763b997bec1809bc280053cffc15f512c9
ccozens <ccozens@gmail.com>
2023-05-18 16:00:52 +0100

    updated vercel env vars and contact api error message

--- src/pages/api/contact/index.ts
+++ src/pages/api/contact/index.ts
@@ -19,7 +19,12 @@ export default async function handler(
 			// set status 200 and return success message
 			res.status(200).json({ message: 'Message sent' });
 		} catch (error) {
-			console.error(error);
-			res.status(500).json({ error: 'Failed to send message' });
+			let message: string = '';
+			if (error instanceof Error) {
+				message = error.message;
+			} else message = String(error);
+			res
+				.status(500)
+				.json({ error: `Failed to create event: ${message}` });
 		}
 }
