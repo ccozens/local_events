43e3d9b08c615798cda9f450fdd4e6a51ffa87a5
ccozens <ccozens@gmail.com>
2023-05-18 14:57:56 +0100

    updated label in min and max age filters on homepage as was not between label tags. Installed playwright

--- src/components/EventOptionToggles.tsx
+++ src/components/EventOptionToggles.tsx
@@ -2,25 +2,35 @@ import styles from '@/styles/Custom.module.css';
 interface EventOptionTogglesProps {
 	setFreeEventsOnly: (freeEventsOnly: boolean) => void;
 	freeEventsOnly: boolean;
-    setNoBookingRequiredOnly: (noBookingRequiredOnly: boolean) => void;
+	setNoBookingRequiredOnly: (noBookingRequiredOnly: boolean) => void;
 	noBookingRequiredOnly: boolean;
 	setMinAge: (minAge: number) => void;
-    setMaxAge: (maxAge: number) => void;
-    setTermOnly: (termOnly: boolean) => void;
+	setMaxAge: (maxAge: number) => void;
+	setTermOnly: (termOnly: boolean) => void;
 	termOnly: boolean;
-};
+}
 
+export default function EventOptionToggles(
+	props: EventOptionTogglesProps
+) {
+	const {
+		freeEventsOnly,
+		setFreeEventsOnly,
+		noBookingRequiredOnly,
+		setNoBookingRequiredOnly,
+		setMinAge,
+		setMaxAge,
+		termOnly,
+		setTermOnly,
+	} = props;
 
-export default function EventOptionToggles(props: EventOptionTogglesProps) {
-    const { freeEventsOnly, setFreeEventsOnly, noBookingRequiredOnly, setNoBookingRequiredOnly, setMinAge, setMaxAge, termOnly, setTermOnly } = props;
+	const minAgeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setMinAge(Number(e.target.value));
+	};
+	const maxAgeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setMaxAge(Number(e.target.value));
+	};
 
-    const minAgeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setMinAge(Number(e.target.value));
-    };
-    const maxAgeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setMaxAge(Number(e.target.value));
-    };		
-    
 	const toggleFree = () => {
 		setFreeEventsOnly(!freeEventsOnly);
 	};
@@ -31,47 +41,50 @@ export default function EventOptionToggles(props: EventOptionTogglesProps) {
 		setTermOnly(!termOnly);
 	};
 
-	const freeOrAll = freeEventsOnly ? 'All events' : 'Free events only';
-	const noBookingOrAll = noBookingRequiredOnly ? 'All events' : 'No booking required';
+	const freeOrAll = freeEventsOnly
+		? 'All events'
+		: 'Free events only';
+	const noBookingOrAll = noBookingRequiredOnly
+		? 'All events'
+		: 'No booking required';
 	const termOrAll = termOnly ? 'All year' : 'Term time only';
 
 	return (
 		<div className={`${styles.eventOptions} ${styles.optionsGrid}`}>
-			<button className={styles.optionButton} onClick={toggleFree}>{freeOrAll}</button>
-			<button className={styles.optionButton} onClick={toggleBookingRequiredEvents}>
+			<button className={styles.optionButton} onClick={toggleFree}>
+				{freeOrAll}
+			</button>
+			<button
+				className={styles.optionButton}
+				onClick={toggleBookingRequiredEvents}>
 				{noBookingOrAll}
 			</button>
-			<button className={styles.optionButton} onClick={toggleTermTimeEvents}>
+			<button
+				className={styles.optionButton}
+				onClick={toggleTermTimeEvents}>
 				{termOrAll}
 			</button>
 			<div className={styles.ageRange}>
-				<p>Age range |</p> 
+				<p>Age range |</p>
 				<div className={styles.option}>
-					<label htmlFor="minAge"></label>
-					Min years
+					<label htmlFor="minAge">Min years</label>
 					<input
+						name="minimum age in years"
 						className={styles.optionInput}
 						type="number"
 						placeholder="min"
-                        onChange={minAgeHandler}
+						onChange={minAgeHandler}
 					/>
-					<label htmlFor="maxAge"></label>
-					Max years
+					<label htmlFor="maxAge">Max years</label>
 					<input
+						name="maximum age in years"
 						className={styles.optionInput}
 						type="number"
 						placeholder="max"
-                        onChange={maxAgeHandler}
+						onChange={maxAgeHandler}
 					/>
 				</div>
 			</div>
 		</div>
 	);
 }
-
-/* todo:
-1. add onclick filter to free to filter events by cost === 0
-2. add onclick filter to no booking required to filter events by booking === false
-3. add onchange filter to age range to filter events by age range
-4. add max === 12 to minAgeMonths and maxAgeMonths on createEvent
- */
