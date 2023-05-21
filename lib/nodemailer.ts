5f7e7e896ceec16e0d2717947cd5584ce9994607
ccozens <ccozens@gmail.com>
2023-05-21 13:35:01 +0100

    updated send email - success message but no email

--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -1,16 +1,72 @@
 import nodemailer, { TransportOptions } from 'nodemailer';
+import { google } from 'googleapis';
 
 interface CustomTransportOptions extends TransportOptions {
 	service: string;
 	auth: {
 		type: string;
 		user: string;
-		pass: string;
+		accessToken: string;
 		clientId: string;
 		clientSecret: string;
 		refreshToken: string;
 	};
 }
+
+const OAuth2 = google.auth.OAuth2;
+
+const createTransporter = async () => {
+	const oauth2Client = new OAuth2(
+		process.env.GMAIL_CLIENT_ID,
+		process.env.GMAIL_CLIENT_SECRET,
+		'https://developers.google.com/oauthplayground'
+	);
+
+	oauth2Client.setCredentials({
+		refresh_token: process.env.GMAIL_REFRESH_TOKEN,
+	});
+
+	const accessToken = await new Promise((resolve, reject) => {
+		oauth2Client.getAccessToken((err, token) => {
+			if (err) {
+				reject();
+			}
+			resolve(token);
+		});
+	});
+
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			type: 'OAuth2',
+			user: process.env.GMAIL_USERNAME,
+			accessToken,
+			clientId: process.env.GMAIL_CLIENT_ID,
+			clientSecret: process.env.GMAIL_CLIENT_SECRET,
+			refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+		},
+	} as CustomTransportOptions);
+
+	return transporter;
+};
+
+interface MailOptions {
+	from: string | undefined;
+	to: string | undefined;
+	subject?: string;
+	text?: string;
+}
+
+export const mailOptions: MailOptions = {
+	from: process.env.GMAIL_USERNAME,
+	to: process.env.GMAIL_USERNAME,
+};
+
+export const sendEmail = async (mailOptions: MailOptions) => {
+	let emailTransporter = await createTransporter();
+	await emailTransporter.sendMail(mailOptions);
+};
+/* 
 
 const authOptions = {
 	type: 'OAuth2',
@@ -30,3 +86,4 @@ export const mailOptions = {
 	from: process.env.GMAIL_USERNAME,
 	to: process.env.GMAIL_USERNAME,
 };
+ */
--- src/pages/api/contact/index.ts
+++ src/pages/api/contact/index.ts
@@ -1,4 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+/* // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { mailOptions, transporter } from '@nodemailer';
 import { getErrorMessage } from '@/functions/getErrorMessage';
@@ -13,6 +13,34 @@ export default async function handler(
 		try {
 			// send email
 			await transporter.sendMail({
+				...mailOptions,
+				subject: `Local events message from ${name}`,
+				text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+			});
+			// set status 200 and return success message
+			res.status(200).json({ message: 'Message sent' });
+		} catch (error) {
+			const message = getErrorMessage(error);
+			res.status(500).json({ error: message });
+		}
+}
+ */
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { mailOptions, sendEmail } from '@nodemailer';
+import { getErrorMessage } from '@/functions/getErrorMessage';
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	// get submitted data from request body
+	const { name, email, message } = req.body;
+	if (req.method === 'POST')
+		try {
+			// send email
+			sendEmail({
 				...mailOptions,
 				subject: `Local events message from ${name}`,
 				text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
